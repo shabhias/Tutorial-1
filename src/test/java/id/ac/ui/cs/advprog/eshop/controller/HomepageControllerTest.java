2dd838635dc4644930968225b0b80bcd9e3b77d4
shabhias <shabhialiyya3101@gmail.com>
2024-02-18 22:08:43 +0700

    add HomepageControllerTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomepageControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomepageControllerTest.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
-class HomepageControllerTest {
+class HomePageControllerTest {
 
     private MockMvc mockMvc;
 
